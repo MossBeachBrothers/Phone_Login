1695f09a7aa415572e1bd58ecc2824a23b153ae6
MossBeachBrothers <mossbeachbrothers@gmail.com>
2023-08-19 17:36:07 -0700

    Login with Email
    08/19/2023

---
+++ Roomies/Roomies/View/LoginEmail.swift
@@ -0,0 +1,22 @@
+//
+//  LoginEmail.swift
+//  Roomies
+//
+//  Created by Akhi Nair on 8/19/23.
+//
+
+
+import SwiftUI
+
+struct LoginEmail : View {
+    var body: some View {
+        Text("hello")
+    }
+}
+
+
+struct LoginEmail_Previews : PreviewProvider {
+    static var previews: some View {
+        LoginEmail()
+    }
+}
